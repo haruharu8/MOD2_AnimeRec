3b3df7a7896871aff345335a4401fff050f9501f
Kelly Siu <senpai@Kellys-MacBook-Pro-2.local>
2023-10-09 23:24:11 -0700

    test

--- src/components/questions/questions.jsx
+++ src/components/questions/questions.jsx
@@ -9,7 +9,7 @@ const Questions = ({answerBreadcrumbs, setanswerBreadcrumbs}) => {
 // √√√ import context to use for future
     const { animeData, setAnimeData,currentHead, setCurrentHead, answersArray, setanswersArray  } = useContext(AnimeDataContext)
 
-    const [currentTails, setCurrentTails] = useState(0);
+    const [animeTitle, setAnimeTitle] = useState('');
 
     let currentObject = undefined;
 
@@ -21,12 +21,36 @@ const Questions = ({answerBreadcrumbs, setanswerBreadcrumbs}) => {
         }
       }
 
+      answersArray.map(() => {})
+
+
       // do api thing here ******
+      // https://api.jikan.moe/v4/anime/{id}/pictures
+      // api for Jikan Moe to get images
       useEffect(() => {
-        if (currentObject.type === 'a'){
-            //make api call using currentObject.tails[0]
+        if (currentObject.type === 'a') {
+          // Make an API call using currentObject.tails[0] (if needed)
         }
-      }, [currentObject])
+      
+        // Use a single fetch statement for the animeTitle API call
+        fetch(`https://api.jikan.moe/v4/anime?q=${animeTitle}&limit=3`)
+          .then((response) => {
+            if (!response.ok) {
+              throw new Error('Network response was not ok');
+            }
+            return response.json();
+          })
+          .then((data) => {
+            // Handle the fetched data as needed
+            console.log(data);
+          })
+          .catch((error) => {
+            console.error('Error fetching data:', error);
+            // Handle the error as needed
+          });
+      }, [animeTitle, currentObject.type]);
+      
+
 
       // √√√ grabs the data(tails) of the button object
       // maybe if I '+'= it will add it the list or we can apped it to an array
@@ -36,10 +60,11 @@ const Questions = ({answerBreadcrumbs, setanswerBreadcrumbs}) => {
         console.log(answerObject) 
         console.log(answerObject.tail) 
         setCurrentHead(answerObject.tail);
+        setAnimeTitle(answerObject.tails);
       }
       // √√√ now the handleClick updates!!!
       // can't read last data type since not num, it is a string****
-
+        console.log(answersArray)
         console.log(answerBreadcrumbs)
       /* √√√   when we click this button (any button), we should change the current head to be whatever this answer's tails is
       
